8ba5a1643437c61335177c1f73530573ce7582c7
Peter Sobot <psobot@gmail.com>
2019-04-15 12:44:40 -0400

    Read from streams in ProcessHelper to avoid deadlock. (#86)

---
+++ spydra/src/test/java/com/spotify/spydra/api/process/ProcessHelperTest.java
@@ -0,0 +1,56 @@
+/*-
+ * -\-\-
+ * Spydra
+ * --
+ * Copyright (C) 2016 - 2018 Spotify AB
+ * --
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -/-/-
+ */
+package com.spotify.spydra.api.process;
+
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+
+public class ProcessHelperTest {
+
+  @Test
+  public void testAllowsForLargeOutputOnStdout() throws IOException {
+    final StringBuilder outputBuilder = new StringBuilder();
+    final boolean success = ProcessHelper.executeForOutput(Arrays.asList(
+        "bash",
+        "-c",
+        "for i in {1..100000}; do echo hello; done"), outputBuilder);
+    assertTrue("Command was not successful: " + outputBuilder.toString(), success);
+    final List<String> words = Arrays.asList(outputBuilder.toString().split("\n"));
+    assertEquals(100000, words.size());
+  }
+
+  @Test
+  public void testAllowsForLargeOutputOnStderr() throws IOException {
+    final StringBuilder outputBuilder = new StringBuilder();
+    final boolean success = ProcessHelper.executeForOutput(Arrays.asList(
+        "bash",
+        "-c",
+        "for i in {1..100000}; do >&2 echo hello; done; false;"), outputBuilder);
+    assertFalse("Command was successful: " + outputBuilder.toString(), success);
+    final List<String> words = Arrays.asList(outputBuilder.toString().split("\n"));
+    assertEquals(100000, words.size());
+  }
+}
